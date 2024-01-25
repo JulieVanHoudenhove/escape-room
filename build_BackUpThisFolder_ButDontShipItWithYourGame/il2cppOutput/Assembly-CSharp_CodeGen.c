b2f4bcbf402ce4a79c8a6e914055e1cc7e2a373c
Jules <j.bousrez@outlook.com>
2024-01-25 16:53:46 +0100

    feat: blank project + working character

---
+++ build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void HandAnimator::Start()
+extern void HandAnimator_Start_mAD38FAE343F9544B1ED35DD63FB08180B9ADFEBC (void);
+// 0x00000002 System.Void HandAnimator::Update()
+extern void HandAnimator_Update_mD34564341AB9D5B653D44E690509B5742DCE4F04 (void);
+// 0x00000003 System.Void HandAnimator::.ctor()
+extern void HandAnimator__ctor_m5C30F9065AF2B28815C50A4A96259028AE4E0242 (void);
+// 0x00000004 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000005 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	HandAnimator_Start_mAD38FAE343F9544B1ED35DD63FB08180B9ADFEBC,
+	HandAnimator_Update_mD34564341AB9D5B653D44E690509B5742DCE4F04,
+	HandAnimator__ctor_m5C30F9065AF2B28815C50A4A96259028AE4E0242,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	5131,
+	5131,
+	5131,
+	5131,
+	5131,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
